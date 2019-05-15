c188966c48abf79c683479d5ddbce90fb84582fa
GGCasual <gc.favorito95@gmail.com>
2019-05-15 08:18:12 -0500

    week 5

--- Cardgame.js
+++ Cardgame.js
@@ -1,63 +1,79 @@
 
-class Deck{
-    constructor() {
-        this.deck = []
-        this.dealt_cards = []
-    }
+var emp_deck = [] 
+var values = ['2','3','4','5','6','7','8','9','10','jack','queen','king','ace']
+var suits = ['clubs', 'diamonds', 'spades', 'hearts']
+var points = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1]
 
 
-    generate_deck() {
-        let card = (suit, value) => {
-            this.name = value + ' of ' + suit
-            this.suit = suit
-            this.value = value
+function getDeck() {
 
-            return {name:this.name, suit:this.suit, value:this.value}
-        }
-        let values = ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A']
-        let suits = ['Clubs', 'Diamonds', 'Spades', 'Hearts']
-
-        for (let s = 0; s < suits.length; s++) {
-            for (let v = 0; v < values.length; v++){
-                this.deck.push(card(suits[s], values[v]))
-            }
+    for (var c = 0; c < values.length; c++) {
+        for (var s = 0; s < suits.length; s++){
+            var img_card = "Playing Cards/PNG-cards-1.3/" + values[c] + '_of_' + suits[s] + '.png';
+            var temp_card = {suits:suits[s], values:values[c], img:img_card, points:points[c]};
+            emp_deck.push(temp_card);
         }
     }
-    print_deck() {
-        if(this.deck.length == 0) {
-            console.log('The deck has been generated')
-        }
-        else {
-            for (let c = 0; c < this.deck.length; c++) {
-                console.log(this.deck[c])
-            }
-        }
-    }
-    shuffle() {
-        let current_ind = this.deck.length, temp_val, rand_ind
-        while (0 != current_ind) {
-            rand_ind = Math.floor(Math.random() * current_ind)
-            current_ind -= 1
-            temp_val = this.deck[current_ind]
-            this.deck[current_ind] = this.deck[rand_ind]
-            this.deck[rand_ind] = temp_val
-        }
-    }
-    deal() {
-        let dealt_card = this.deck.shift()
-        this.dealt_cards.push(dealt_card)
-        return dealt_card
-    }
-    replace() {
-        this.deck.unshift(this.dealt_cards.shift())
-    }
-    clear_deck() {
-        this.deck = []
-    }
+    return emp_deck;
 }
 
-deck = new Deck()
+getDeck();
 
-deck.generate_deck()
-deck.shuffle()
-deck.print_deck()
+var dealerHand = []
+var playerHand = []
+
+
+function deal() {
+
+    var card = emp_deck.pop();
+    playerHand.push(card);
+    var card = emp_deck.pop();
+    dealerHand.push(card);
+    var card = emp_deck.pop();
+    playerHand.push(card);
+    var card = emp_deck.pop();
+    dealerHand.push(card);
+
+    var playerCard1 = document.createElement('img')
+    playerCard1.setAttribute('src', playerHand[0].img)
+    var playerCard2 = document.createElement('img')
+    playerCard2.setAttribute('src', playerHand[1].img)
+    var dealerCard1 = document.createElement('img')
+    dealerCard1.setAttribute('src', dealerHand[0].img)
+    var dealerCard2 = document.createElement('img')
+    dealerCard2.setAttribute('src', dealerHand[1].img)
+
+    var playerTable = document.getElementById('player-hand')
+    var dealerTable = document.getElementById('dealer-hand')
+    playerTable.appendChild(playerCard1)
+    playerTable.appendChild(playerCard2)
+    dealerTable.appendChild(dealerCard1)
+    dealerTable.appendChild(dealerCard2)
+
+}
+
+deal();
+
+
+function calcPoints() {
+    
+}
+
+
+
+
+
+// document.getElementById("deal-button").addEventListener("click", function(){
+//     deck.deal()
+
+// });
+
+
+// document.getElementById("hit-button").addEventListener("click", function(){
+
+// });
+
+
+// document.getElementById("stand-button").addEventListener("click", function(){
+
+// });
